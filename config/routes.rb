1d6e44ed97352873745411f0e5c4dc88155d35c7
Harjot Kainth <harjot@kainth.ca>
2017-11-12 20:34:44 +0000

    added Customer controller/views and styles - Part 2 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'customers#index', as: 'customers'
+
+  get 'customers/index'
+  get 'customers/alphabetized', as: 'alphabetized'
+
+  get 'customers/missing_email', as: 'missing_emails'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
