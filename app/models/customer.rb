dd713bae1d097e33c3ccc724b21217f42c867a24
Harjot Kainth <harjot@kainth.ca>
2017-11-12 22:18:16 +0000

    finished adding provinces column and model, including updated customer view. - part 3 done.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ApplicationRecord
+    belongs_to :province
+
     validates :full_name, presence: true
     validates :full_name, uniqueness: true
     validates :phone_number, length: {minimum: 7}
